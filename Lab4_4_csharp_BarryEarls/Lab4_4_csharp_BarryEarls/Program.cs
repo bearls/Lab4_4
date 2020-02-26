1c08f0eb86d85843feac117282d98c6793831ea6
bearls <61299567+bearls@users.noreply.github.com>
2020-02-25 18:55:24 -0600

    Add files via upload

---
+++ Lab4_4_csharp_BarryEarls/Lab4_4_csharp_BarryEarls/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab4_4_Csharp_BarryEarls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] blackListWord = { "buy", "viagra", "XXX", "free money",
+            "lifetime offer", "send money", "bank account", "nigeria",
+            "online pharmacy", "h8te", "meet girls" };
+
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            message = message.ToLower();
+
+            for (int i = 0; i < blackListWord.Length; i++)
+            {
+                if (message.Contains(blackListWord[i]))
+                {
+                    isSpam = true;
+                }
+            }
+
+
+            if (isSpam == true)
+            {
+
+                Console.WriteLine("The message contained spam");
+            }
+            else
+            {
+
+                Console.WriteLine("The Message Is Ok.");
+            }
+
+
+
+        }
+    }
+}
